6fb09219e8c8e6c0935e0796e8ad2ab673d12e82
Lucas Ethan <asmrit1010@gmail.com>
2021-08-03 14:00:50 +0200

    Added prettier formatter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-const Pet = ({name, animal, breed}) => {
-  return React.createElement("div", {}, [React.createElement("h1", {}, name), React.createElement("h2", {}, animal), React.createElement("h2", {}, breed)])
-}
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
 
 const App = () => {
-  return React.createElement("div", {}, [React.createElement("h1", {}, "Adopt me!"), React.createElement(Pet, {name: "Luke", animal: "dog", breed: "Auggie"}), React.createElement(Pet, {name: "Max", animal: "Rabbit", breed: "Belgian Hare Rabbit"}), React.createElement(Pet, {name: "Charlie", animal: "Hamster", breed: "Darf White Russian Hamster"})])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, { name: "Luke", animal: "dog", breed: "Auggie" }),
+    React.createElement(Pet, {
+      name: "Max",
+      animal: "Rabbit",
+      breed: "Belgian Hare Rabbit",
+    }),
+    React.createElement(Pet, {
+      name: "Charlie",
+      animal: "Hamster",
+      breed: "Darf White Russian Hamster",
+    }),
+  ]);
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
