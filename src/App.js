adfdb9b7faeefb60806f38c0e8ef3a7828ffdc04
Lucas Ethan <asmrit1010@gmail.com>
2021-08-07 10:18:00 +0200

    Javascript tooling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet"
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, { name: "Luke", animal: "dog", breed: "Auggie" }),
+    React.createElement(Pet, { name: "Luke", animal: "Dog", breed: "Auggie" }),
     React.createElement(Pet, {
       name: "Max",
       animal: "Rabbit",
