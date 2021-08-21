acb29cfbbff328cdf91c69b81f87ff8d2c7417c9
Lucas Ethan <asmrit1010@gmail.com>
2021-08-21 10:44:48 +0200

    Added SearchParams component

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,16 +1,59 @@
+import { useState } from "react";
+
 const SearchParams = () => {
-  const location = "Seattle, WA"
-  return  (
+  const ANIMALS = Object.freeze(["bird", "dog", "cat", "rabbit", "reptile"]);
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [animal, updateAnimal] = useState("");
+  const [breed, updateBreed] = useState("");
+  const breeds = [];
+
+  return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location"/>
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => updateLocation(e.target.value)}
+          />
         </label>
-        <input type="submit" value="Submit"/>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onBlur={(e) => updateAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onBlur={(e) => updateBreed(e.target.value)}
+            disabled={!breeds.length}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SearchParams
+export default SearchParams;
